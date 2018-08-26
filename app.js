90a693012cf84b301f0ceadf994e6ba935f3f8a4
Aidan Gadberry <agadberr@gmail.com>
2018-08-25 23:38:56 -0700

    Basic node project setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
+
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+app.get('/', (req, res) => res.send("Hello World!"));
+app.use('/api/users', users);
+app.use('/api/events', events);
+
+// Middleware for body parse
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
